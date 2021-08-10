899978683b80fed72700b6a83c714aefae66fb34
Benjamin Mandervelde <benjaminmanderveldepro@gmail.com>
2021-08-10 20:07:21 +0200

    feat: add GraphRequest execution exception throwed when status code is not 200

---
+++ src/Exceptions/GraphRequestExecutionException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Net.Http;
+
+namespace Client
+{
+	public class GraphRequestExecutionException : Exception
+	{
+		public GraphRequestExecutionException(HttpResponseMessage message) : base("An error happened while trying to process the http request, see the ResponseMessage variable.")
+		{
+			ResponseMessage = message;
+		}
+
+		public HttpResponseMessage ResponseMessage { get; private set; }
+	}
+}
