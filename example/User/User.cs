f7cc66b3ab4f9d9649849b88b93ef82dccca4fea
Benjamin Mandervelde <benjaminmanderveldepro@gmail.com>
2021-08-13 11:57:11 +0200

    feat: update example

--- example/User/User.cs
+++ example/User/User.cs
@@ -6,13 +6,14 @@ namespace TestClient.User
 	public class User
 	{
 		[GraphPropertyName("name")]
+		[GraphUnionTypeProperty(typeof(int), typeof(string))]
 		public string Name { get; set; }
 		
 		[GraphPropertyName("username")]
 		public string Username { get; set; }
 
 		[GraphPropertyName("posts")]
-		public List<Post.Post> Posts([GraphNonNullableProperty] [GraphPropertyName("postsId")] int id) => new List<Post.Post>();
+		public List<Post.Post> Posts([GraphNonNullableProperty] [GraphPropertyName("postsId")] [GraphPropertyTypeAttribute(typeof(string))] int id) => new List<Post.Post>();
 		
 		[GraphPropertyName("comments")]
 		public List<Comment.Comment> Comments { get; set; }
