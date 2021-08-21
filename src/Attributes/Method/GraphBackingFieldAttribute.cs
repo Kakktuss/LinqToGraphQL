43e6ae36f886299f4e6772bc41ae3c5fc8d01b66
Benjamin Mandervelde <benjaminmanderveldepro@gmail.com>
2021-08-21 11:46:50 +0200

    feat: Add new GraphBackingField attribute to work on JSON deserialization

---
+++ src/Attributes/Method/GraphBackingFieldAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace LinqToGraphQL.Attributes.Method
+{
+	[AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+	public class GraphBackingFieldAttribute : Attribute
+	{
+		public GraphBackingFieldAttribute(string backingField)
+		{
+			BackingField = backingField;
+		}
+		
+		public string BackingField { get; set; }
+	}
+}
--- src/Json/ContractResolvers/GraphPropertyContractResolver.cs
+++ src/Json/ContractResolvers/GraphPropertyContractResolver.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using LinqToGraphQL.Attributes;
+using LinqToGraphQL.Attributes.Method;
 using LinqToGraphQL.Json.Converters;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
@@ -11,10 +12,48 @@ namespace LinqToGraphQL.Json
 {
 	public class GraphPropertyNameContractResolver : DefaultContractResolver
 	{
+		private readonly Dictionary<string, MemberInfo> _methodBackingFields = new();
+
+		protected override List<MemberInfo> GetSerializableMembers(Type objectType)
+		{
+			var serializableMembers = base.GetSerializableMembers(objectType);
+
+			var objectMembers = objectType.GetMembers(BindingFlags.NonPublic | BindingFlags.Instance);
+			
+			foreach (var member in objectMembers.Where(e => e is MethodInfo 
+			                                                && !e.Name.StartsWith("get_") 
+			                                                && !e.Name.StartsWith("set_") 
+			                                                && !e.Name.StartsWith("k__")))
+			{
+				var graphDelegateValueAttribute = member.GetCustomAttributesData().FirstOrDefault(e => e.AttributeType == typeof(GraphBackingFieldAttribute));
+
+				if (graphDelegateValueAttribute is not null)
+				{
+					var propertyBackingField = objectMembers.Where(e => e.Name == (string) graphDelegateValueAttribute.ConstructorArguments.FirstOrDefault().Value)?.FirstOrDefault();
+
+					if (propertyBackingField is not null)
+					{
+						_methodBackingFields.Add(propertyBackingField.Name, member);
+					
+						serializableMembers.Add(propertyBackingField);
+					}
+				}
+			}
+			
+			return serializableMembers;
+		}
+
 		protected override JsonProperty CreateProperty(MemberInfo member, MemberSerialization memberSerialization)
 		{
 			var property = base.CreateProperty(member, memberSerialization);
 
+			if (_methodBackingFields.TryGetValue(property.PropertyName, out var originalMethod))
+			{
+				property.PropertyName = originalMethod.Name;
+
+				member = originalMethod;
+			}
+			
 			var graphNameAttribute = member.GetCustomAttributesData().FirstOrDefault(e => e.AttributeType == typeof(GraphNameAttribute));
 
 			var graphUnionTypeAttribute = member.GetCustomAttributesData().FirstOrDefault(e => e.AttributeType == typeof(GraphUnionTypePropertyAttribute));
